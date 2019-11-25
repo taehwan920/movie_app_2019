65a413cfefed436e9c00061fef7c7298704c39c7
taehwan920 <56597998+taehwan920@users.noreply.github.com>
2019-11-25 20:24:15 +0900

    axios, api, async/await

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
 import React from 'react';
+import axios from "axios";
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
-  add = () => {
-    /*this.state.count=1;은 안됨. 직접적으로 state를 변경하는건 안됨. 매번 "새로 설정"한다는 느낌으로 아래와같이 ㄱ */
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+  }
   componentDidMount() {
-    console.log("component rendered");
-  }
-  /* componentDidMount는 render가 실행되고 나서 실행됨. */
-  componentDidUpdate() {
-    console.log("I just updated~!");
-  }
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world")
+    this.getMovies();
   }
   render() {
-    console.log("I am rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
-  };
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
-/*위의 extends == extends from 즉  React > App*/
-/*함수가 아니라 class라 return대신 render를 이용.*/
 
 export default App;
