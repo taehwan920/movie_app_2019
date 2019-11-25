6bb89c345ec6967330132e7cc42e46788d06764f
taehwan920 <56597998+taehwan920@users.noreply.github.com>
2019-11-25 22:29:07 +0900

    make html tag & import css

--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css"
 
 /*이번 케이스는 state가 필요한 것이 아니니 function component로 간다 */
-function Movie(id, year, title, summary, poster) {
-    return <h4>{title}</h4>
+function Movie({ year, title, summary, poster }) {
+    return <div class="movies__movie">
+        <img src={poster} alt={title} title={title} />
+        <div class="movie__data">
+            <h3 class="movie__title">{title}</h3>
+            <h5 class="movie__year">{year}</h5>
+            <p class="movie__summary">{summary}</p>
+        </div>
+    </div>;
 }
 
 Movie.propTypes = {
